b1354a461d118d57ec14439b549607c4facd9a22
wasnot <wasnot.apps@gmail.com>
2020-08-26 20:00:37 +0900

    add android lib

---
+++ Sample/Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using IO.Karte.Android.Tracking;
+
+namespace SampleApp.Droid
+{
+    [Activity(Label = "SampleApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate {
+                Tracker.Track("countup");
+                Tracker.View("popup");
+                button.Text = $"{count++} clicks!";
+            };
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Tracker.View("main");
+        }
+    }
+}
+
---
+++ Sample/SampleApp/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SampleApp
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
