1a55561ce9ac8851ec2cf98d830813ae6f2340e1
kota.fujiwara <kota.fujiwara@plaid.co.jp>
2022-07-01 17:24:40 +0900

    refine version update logic

--- scripts/ci/update_versions.rb
+++ scripts/ci/update_versions.rb
@@ -5,13 +5,24 @@ require 'rexml/document'
 
 require_relative './utils'
 
+def increment_revision(version)
+  versions = version.split('.')
+  if versions.size == 3
+    versions << '0'
+  elsif versions.size == 4
+    versions[3] = (versions[3].to_i + 1).to_s
+  end
+  versions.join('.')
+end
+
 script_dir_path = File.dirname($0)
 tmp_dir_path = '../../tmp'
 Dir.chdir(script_dir_path)
 
 # Update versions of iOS modules
-
 ios_latest_versions = get_ios_latest_versions
+ios_core_version_changed = false
+ios_any_module_version_changed = false
 IOS_MODULE_NAMES.each do |module_name|
   nuspec_path = "../../karte-component/nuget/Karte.iOS.#{module_name}.nuspec"
   nuspec = File.read(nuspec_path)
@@ -23,7 +34,8 @@ IOS_MODULE_NAMES.each do |module_name|
   if current_version != ios_latest_versions[module_name]
     REXML::XPath.first(doc, '//version').text = ios_latest_versions[module_name]
     doc_changed = true
-  end
+    ios_core_version_changed = true if module_name == 'Core'
+    ios_any_module_version_changed = true end
 
   REXML::XPath.each(doc, '//dependency') do |dep|
     IOS_MODULE_NAMES.each do |dep_module|
@@ -38,13 +50,18 @@ IOS_MODULE_NAMES.each do |module_name|
 end
 
 # Update KarteXamarin version(iOS)
+ios_new_version = ios_latest_versions['Core']
+ios_new_version = increment_revision(ios_new_version) unless ios_core_version_changed
+
 karte_xamarin_source_path = "../../karte-component/src/ios-unified/source/KarteXamarin/KarteXamarin/KarteXamarin.m"
 karte_xamarin_source = File.read(karte_xamarin_source_path)
-karte_xamarin_source.gsub!(/(return.*@")[0-9.]+"/, "\\1#{ios_latest_versions['Core']}\"")
+karte_xamarin_source.gsub!(/(return.*@")[0-9.]+"/, "\\1#{ios_new_version}\"")
 File.open(karte_xamarin_source_path, 'w') { |f| f.write(karte_xamarin_source) }
 
 # Update version of Android modules
 android_latest_versions = get_android_latest_versions
+android_core_version_changed = false
+android_any_module_version_changed = false
 ANDROID_MODULE_NAMES.each do |module_name|
   csproj_path = "../../Karte.Android/Karte.Android.#{module_name}/Karte.Android.#{module_name}.csproj"
   csproj = File.read(csproj_path)
@@ -57,11 +74,18 @@ ANDROID_MODULE_NAMES.each do |module_name|
     File.open(csproj_path, 'w') do |f|
       doc.write(f)
     end
+    android_core_version_changed = true if module_name == 'Core'
+    android_any_module_version_changed = true
   end
 end
 
 ## Update KarteXamarin version(Android)
+android_new_version = android_latest_versions['Core']
+if !android_core_version_changed && android_any_module_version_changed
+  android_new_version = increment_revision(android_new_version)
+end
+
 karte_xamarin_source_path = "../../Karte.Android/Karte.Android.Core/Additions/XamarinLibrary.cs"
 karte_xamarin_source = File.read(karte_xamarin_source_path)
-karte_xamarin_source.gsub!(/(Version.*=>.*")[0-9.]+"/, "\\1#{android_latest_versions['Core']}\"")
+karte_xamarin_source.gsub!(/(Version.*=>.*")[0-9.]+"/, "\\1#{android_new_version}\"")
 File.open(karte_xamarin_source_path, 'w') { |f| f.write(karte_xamarin_source) }
